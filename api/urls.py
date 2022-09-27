945bcf1694d6ab0d0184b840f8a2aa8b8290d0ee
PradeepKumar <PradeepKumarRebbavarapu705@gmail.com>
2022-09-28 00:07:42 +0530

    'i

---
+++ api/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path,include
+from .views import *
+urlpatterns = [
+    path('Login/',Login.as_view()),
+    path('Signup/',Signup.as_view()),
+    path('ListAddProduct/',ListAddProduct.as_view()),
+    path('RetrieveUpdateDestroyProduct/<int:pk>/',RetrieveUpdateDestroyProduct.as_view()),
+    path('filter',CustomFilter.as_view()),
+    path('GetAllElementsFromKart/',GetAllElementsFromKart.as_view()),
+    path('AddElementToKart/',AddElementToKart.as_view()),
+    path('DeleteElementFromKart/',DeleteKartElement.as_view()),
+    path('PlaceOrderThruCart/',PlaceOrderThroughCart.as_view()),
+    path('handlerequest/',handelrequest.as_view())
+    
+]
